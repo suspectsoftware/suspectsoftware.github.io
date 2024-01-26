0e880ec102a21090bd6e52b822843bdcd3963eab
Rasmus Bo Jørgensen <daerjo@egmont.com>
2024-01-26 08:14:38 +0100

    Disable image optimization

--- next/web/app/page.tsx
+++ next/web/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24" style={{ fontSize: '50px' }}>
       <div className="z-10 max-w-5xl w-full flex items-center justify-center font-merriweather lg:flex">
-        <Image src="/logo.png" alt="Logo" width={100} height={100} />
+        <Image src="/logo.png" alt="" width={100} height={100} />
         <p className="pl-2">SuspectSoftware</p>
       </div>
     </main>
   );
-}
+} 
